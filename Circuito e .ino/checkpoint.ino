a1de0d67b223ddb9c3f4b27b9465d8176874230a
Eric Pessoa <61791018+Eric-Pessoa@users.noreply.github.com>
2021-04-09 18:44:56 -0300

    Upload parte quase pronta pra giba
    NÃO PULA

--- Circuito e .ino/checkpoint.ino
+++ Circuito e .ino/checkpoint.ino
@@ -1,16 +1,20 @@
 #include <Adafruit_SSD1306.h>
+//#include "CountDown.h"C
 #define OLED_Address 0x3c
 Adafruit_SSD1306 oled(128, 64);
+//CountDown CD(CountDown::MINUTES);
 
 
 //Declarando variáveis para facilitar o código por nome
 // e não pelos números
 
 int x=0, lastx=0, lasty=60, mais = 2, menos = 3, enter = 4, bomba = 9, 
-aquecedor = 10, som = 11, led = 13, minDisplay = 0, minDisplayLast = 0,
-TermistorPin = A0, tempSetada = 55, Vo;
+aquecedor = 10, som = 11, led = 13, minDisplay = 0,
+TermistorPin = A0,     tempSetada = 20,       Vo;
 
-float R1 = 10000, logR2, R2, T, Tc, Tf, temp, templast, timerlast,
+int enterPressionado =  0;
+
+float R1 = 10000, logR2, R2, T, Tc, Tf, temp,
 c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
 
 
@@ -25,7 +29,6 @@ void setup(){
  	oled.clearDisplay();
   	oled.setTextSize(1);
   	oled.setTextColor(WHITE);
-
 	pinMode(mais, INPUT_PULLUP);
 	pinMode(menos, INPUT_PULLUP);
 	pinMode(enter, INPUT_PULLUP);
@@ -40,64 +43,158 @@ void setup(){
 
 
 void loop(){
+
+    //começa pela temperatura
+	//Tem que ficar no começo
+	
+	//não pula
+	
+	
+	//OLHA!!
+	
+	
+	//Gi grossa
+	//temperatura do mundo real;
 	temp = readTemp(TermistorPin);
-	oledDisplay(temp, minuto, minDisplay);
+	oledDisplay(temp, minuto, tempoEnter, minDisplay);
 	
-//capturar minuto. A variável se chama minuto mas o código é
-//calculado em milissegundos
-//Se eu apertar o botão "+" eu adiciono 1 minuto no timer
-	if(digitalRead(mais) == LOW){
-		minuto = minuto + 60000;
-		minDisplay = minDisplay + 1;
-
-//variável display para iminutar um timer. a cada 60 000 ms (1 minuto)
-//adiciona 1 unidade nessa variável até 59
-
-	if (minDisplay > 59){
-		minDisplay = 0;
+	//PUTZ LARI
+	//!!!!!!!!!
+	//Arrumar
+	if(enterPressionado == 0) {
+	
+		if(digitalRead(mais) == LOW) {
+			tempSetada += 5;
 		}
-	}
-
-// Igual à variável acima só que com o botão "-"
-
-	if(digitalRead(menos) == LOW && minuto > 0){
-		minuto = minuto - 60000;
-		minDisplay = minDisplay - 1;
-
-
-	if (minDisplay < 0){
-		minDisplay = 59;
+		
+		if(digitalRead(menos) == LOW) {
+			tempSetada -= 5;
 		}
-	}
-
-	if(temp>0 && digitalRead(enter) == LOW && minuto >= 60000){
-		digitalWrite(bomba, HIGH);
-		digitalWrite(aquecedor, HIGH);
-	}
 
 
-	if (temp>=tempSetada){
-		digitalWrite(aquecedor, LOW);
-	}
+		//DANNNNN
+		// to
+		if(digitalRead(enter) == LOW) {
+			enterPressionado += 1;
+		}
 
-	else if(temp<tempSetada && digitalRead(enter) == LOW){
-		digitalWrite(bomba, HIGH);
-		digitalWrite(aquecedor, HIGH);
 	}
 	
-	else if(digitalRead(bomba)== HIGH && digitalRead(aquecedor) == LOW && temp<tempSetada){
-		digitalWrite(aquecedor, HIGH);
-	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
-	if(digitalRead(enter) == LOW){
-		tempoEnter = millis();
-	}
+
+	//loading...
 
 
+	//Situações  
+	if(enterPressionado == 1) {
+
+	//É ISSO!!!!!
+	//TAÍ!!!
+				
+		//capturar minuto. A variável se chama minuto mas o código é
+		//calculado em milissegundos
+		//Se eu apertar o botão "+" eu adiciono 1 minuto no timer
+			if(digitalRead(mais) == LOW){
+				minuto = minuto + 60000;
+				minDisplay = minDisplay + 1;
+		
+		//variável display para iminutar um timer. a cada 60 000 ms (1 minuto)
+		//adiciona 1 unidade nessa variável até 59
+		
+				if (minDisplay > 59){
+					minDisplay = 0;
+				}
+			}
+		
+		// Igual à variável acima só que com o botão "-"
+		
+			if(digitalRead(menos) == LOW && minuto > 0){
+				minuto = minuto - 60000;
+				minDisplay = minDisplay - 1;
+				
+		
+			//setar pra depois que acabar
+				if (minDisplay < 0){
+					minDisplay = 59;
+				}
+				
+				
+			
+			}
+			
+			if(digitalRead(enter) == LOW) {
+			enterPressionado += 1;
+			}
+			
+			//CARALHO GI!!!!!!!!! 
+			// MT VDD
+			//até esse? :(
+		}
+		
+		
+		
+		
+		
+		
+		else if(enterPressionado == 2 && minuto >= 1000) {
+		
+					//caso já tenha temperatura e tempo
+			if(temp < tempSetada && digitalRead(enter) == LOW && minuto >= 60000){
+				digitalWrite(bomba, HIGH);
+				digitalWrite(aquecedor, HIGH);
+			}
+		
+			
+			if (temp>=tempSetada){
+				digitalWrite(aquecedor, LOW);
+			}
+		
+			
+			//tirei && digitalRead(enter) == LOW
+			else if(temp<tempSetada){
+				digitalWrite(bomba, HIGH);
+				digitalWrite(aquecedor, HIGH);
+			}
+			
+			else if(digitalRead(bomba)== HIGH && digitalRead(aquecedor) == LOW && temp<tempSetada){
+				digitalWrite(aquecedor, HIGH);
+			}
+		
+		
+		}
+	
+	//BOA GIIIIIIIIIII
+	//!!!!!!!!!!!!!!! 
+		
+	
+	
+	
+
+
+
+	//pra acabar
+	
+	
+	
+	
+	
+	
+	
+	
 	if(millis() - tempoEnter >= minuto && minuto >= 60000){
 		digitalWrite(aquecedor, LOW);
 		digitalWrite(bomba, LOW);
-
+		}
 
 	if(digitalRead(led) == LOW && digitalRead(som) == LOW){
 		digitalWrite(led, HIGH);
@@ -108,7 +205,24 @@ void loop(){
 		digitalWrite(led, LOW);
 		digitalWrite(som, LOW);
 		}
-	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//Serials
+	
+	//Tempo Setado, tempo atual, temperatura setada, temperatura medida, status da bomba
+	//status do aquecedor, status putaquepariu
+	
+	
+	
 
 	
 	//imprime o tempo desde que o Arduino foi ativado na eletricidade
@@ -119,16 +233,26 @@ void loop(){
 	//Divido por 3600 e transformo em horas.
 	Serial.print("Hora: ");Serial.println(minuto/1000/3600);
 	//Imprimir temperatura
-	printTemp(temp);
+	//printTemp(temp);
+	Serial.print("REGRESSIVA: ");Serial.println(minuto/1000-(millis()-tempoEnter)/1000);
 	//Imprimir temperatura setada
 	Serial.print("Temperatura setada: ");Serial.print(tempSetada);Serial.println(" C");
-	Serial.print("Status bomba: ");Serial.println(digitalRead(bomba));
-	Serial.print("Status aquecedor: ");Serial.println(digitalRead(aquecedor));
+	//Temperatura da água no mundo real
+	Serial.print("Temperatura da água na realidade: ");Serial.print(temp);Serial.println("  C");
+	//Serial.print("Status bomba: ");Serial.println(digitalRead(bomba));
+	//Serial.print("Status aquecedor: ");Serial.println(digitalRead(aquecedor));
 	//Ciclo do arduino a cada 60 segundo para sincronizar com os timers;
-	
 	delay(1000);
+
 	
 	}
+
+
+
+
+
+//Configurações readtemp e mostrar no Oled
+
 
 float readTemp(int ThermistorPin){	
 	Vo = analogRead(ThermistorPin);
@@ -148,19 +272,48 @@ void printTemp(float Tc){
 	
 }
 
-void oledDisplay(float temp, unsigned long minuto, int minDisplay){
+
+
+	//temperatura setada pelo usuario temperatura atual e timer -- visor
+
+void oledDisplay(float temp, unsigned long minuto, unsigned long tempoEnter, int minDisplay){
+	unsigned long tempoEnterOled;
 	oled.clearDisplay();
 	
 	oled.setCursor(0,0);
 	oled.setTextColor(WHITE);
-	oled.print("Temp: ");
-	oled.println(temp);
-	templast = temp;
-	
+	oled.print("Temperatura: ");
+	oled.print(temp);
+	oled.print("C: ");
 	oled.println("");
 	oled.print("Timer:  ");
 	oled.print(minuto/1000/3600);oled.print(":");oled.print(minDisplay);
-	timerlast = minuto/1000/3600;
-	minDisplayLast = minDisplay;
-	oled.display();
+	unsigned long seg, min, hor;
+	seg = minuto/1000-(millis()-tempoEnter)/1000;
+	min = minuto/60000-(millis()-tempoEnter)/60000;
+	hor = minuto/3600000-(millis()-tempoEnter)/3600000;
+	int segDisplay = 100;
+	int minuDisplay;
+	
+	
+	if(minuto/1000-(millis()-tempoEnter)/1000> 59000){
+		segDisplay = 59;
+		segDisplay--;
+		Serial.print("TESTE: ");Serial.println(segDisplay);
+		}
+	
+	
+	
+	
+	if(digitalRead(bomba) == HIGH){
+		oled.println("");
+		oled.print("Tempo restante: ");
+		oled.println("");
+		oled.print(hor);oled.print(":");oled.print(min);oled.print(":");oled.print(segDisplay);
+		}
+		
+		
+		oled.display();
 }
+
+
